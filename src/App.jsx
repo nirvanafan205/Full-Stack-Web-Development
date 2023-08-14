6ab014f64543800dc2423dd0d030aae17da47308
nirvanafan205 <nirvanafan205@gmail.com>
2023-08-13 21:16:34 -0700

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-//******Import Statements*******
 import React from "react";
-import { createRoot } from "react-dom/client"; // rool-lvl rendering
+import ReactDOM from "react-dom"; // Correct the import statement
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./LandingPage";
 import "./style.css";
 
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+      </Switch>
+    </Router>
+  );
 };
 
-// finds an HTML elment with the id "root"
-const container = document.getElementById("root");
-
-// createRoot create a new root for rendering
-const root = createRoot(container);
-
-// render method of the root is called with the 'App' component as its argument
-// renders the entier application
-root.render(<App />);
-
-/*
-    sets up a React application with routing and data fetching capabilities 
-    using React Router and React Query
-*/
+// Render the app component using ReactDOM.render
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Lab 3!</h1>
+      <h2>
+        Let's figure out who you are, login in or create a username please!
+      </h2>
+    </div>
+  );
+};
+
+export default LandingPage;
