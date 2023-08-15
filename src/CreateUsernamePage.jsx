4ab306f9abd90bb97a54500b2fb98b9a0782be2e
nirvanafan205 <nirvanafan205@gmail.com>
2023-08-14 22:02:36 -0700

    validated password for atleast one number

--- src/CreateUsernamePage.jsx
+++ src/CreateUsernamePage.jsx
@@ -5,121 +5,103 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 
 const CreateUsernamePage = () => {
+  const [username, setUsername] = useState("");
+  const [isUsernameValid, setIsUsernameValid] = useState(true);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
 
-	// username
-	const [username, setUsername] = useState("");
-	const [isUsernameValid, setIsUsernameValid] = useState(true);
-	const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [password, setPassword] = useState("");
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [showPasswordErrorMessage, setShowPasswordErrorMessage] =
+    useState(false);
 
-	// password
-	const [password, setPassword] = useState("");
-	const [isPasswordValid, setIsPasswordValid] = useState(true);
-	const [showPasswordErrorMessage, setShowPasswordErrorMessage] = useState(false);
+  const handleUsernameChange = (event) => {
+    const newUsername = event.target.value;
+    const isValid = /^[a-z]*\d{3,}$/.test(newUsername);
 
-	const handleUsernameChange = (event) => {
-		const newUsername = event.target.value;
+    setUsername(newUsername);
+    setIsUsernameValid(isValid);
+    setShowErrorMessage(!isValid && newUsername !== "");
 
-		// Validate the username
-		const isValid = /^[a-z]*\d{3,}$/.test(newUsername);
+    const lowercaseLettersOnly = /^[a-z]+$/.test(newUsername);
+    const atLeastThreeNumbers = (newUsername.match(/\d/g) || []).length >= 3;
+    setIsUsernameValid(lowercaseLettersOnly && atLeastThreeNumbers);
+  };
 
-		// set state values
-		setUsername(newUsername);
-		setIsUsernameValid(isValid);
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    const isValid = /^[a-z]*\d+$/.test(newPassword); // Validate lowercase letters and at least one number
 
+    setPassword(newPassword);
+    setIsPasswordValid(isValid);
+    setShowPasswordErrorMessage(newPassword !== "" && !isValid);
+  };
 
-		setShowErrorMessage(!isValid && newUsername !== "");
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-		// Validate the username
-		const lowercaseLettersOnly = /^[a-z]+$/.test(newUsername);
-		const atLeastThreeNumbers = (newUsername.match(/\d/g) || []).length >= 3;
-		setIsUsernameValid(lowercaseLettersOnly && atLeastThreeNumbers);
-	};
+    if (!isUsernameValid) {
+      return;
+    }
+  };
 
-	const handlePasswordChange = (event) => {
-		const newPassword = event.target.value;
-	  
-		// Validate the password
-		const lowercaseLettersOnly = /^[a-z]+$/.test(newPassword);
-		const atLeastOneNumber = /\d/.test(newPassword);
-		const atLeastOneSpecialCharacter = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(newPassword);
-		const isValid = lowercaseLettersOnly && atLeastOneNumber && atLeastOneSpecialCharacter;
-	  
-		// Set state values
-		setPassword(newPassword);
-		setIsPasswordValid(isValid);
-		setShowPasswordErrorMessage(!isValid);
-	  };
-	  
-  
+  return (
+    <div>
+      {/* Navigation Bar */}
+      <div className="navBar">
+        <Link to="/" className="home-button">
+          <FontAwesomeIcon icon={faHouse} />
+        </Link>
+      </div>
 
+      <div className="login-form">
+        <div className="color">
+          <h1 className="LoginPost">Registration</h1>
+          <form action="#" method="post">
+            <div className="inputWrapper">
+              <p>User Name</p>
+              <input
+                className={`input${!isUsernameValid ? " invalid" : ""}`}
+                type="text"
+                name="user"
+                placeholder="User Name"
+                value={username}
+                onChange={handleUsernameChange}
+              />
 
+              <p>Password</p>
+              <input
+                className={`input${!isPasswordValid ? " invalid" : ""}`}
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
+            {showErrorMessage && (
+              <p className="error-message">
+                User name must contain only lowercase letters and at least three
+                numbers.
+              </p>
+            )}
 
-		if(!isUsernameValid) {
-			return;
-		}
-	};
+            {showPasswordErrorMessage && (
+              <p className="error-message">
+                Password must contain lowercase letters and at least one number.
+              </p>
+            )}
 
-	return (
-		<div>
-		{/* Navigation Bar */}
-		<div className="navBar">
-		<Link to="/" className="home-button">
-		<FontAwesomeIcon icon={faHouse} />
-		</Link>
-		</div>``
-
-		{/* Login Form */}
-		<div className="login-form">
-		<div className="color">
-		<h1 className="LoginPost">Registration</h1>
-		<form action="#" method="post">
-		<div className="inputWrapper" >
-		<p>User Name</p>
-
-		<input 
-		className={`input${!isUsernameValid ? "invalid": ""}`}
-		type="text" 
-		name="user" 
-		placeholder="User Name" 
-		value={username}
-		onChange={handleUsernameChange}
-		/>
-
-		<p>Password</p>
-		<input 
-		className={`input${!isPasswordValid ? " invalid" : ""}`}
-		type="password" 
-		name="password" 
-		placeholder="Password"
-		value={password}
-		onChange={handlePasswordChange}
-		/>
-		</div>
-
-		{showErrorMessage && (
-			<p className="error-message">
-			User name must contain only lowercase letters and at least three numbers.
-			</p>
-		)}
-
-		{showPasswordErrorMessage && (
-			<p className="error-message">
-			Password must contain lowercase letters, at least one number, and one special character.
-			</p>
-		)}
-
-		<div className="wrapper">
-		<button className="loginButton" type="submit">Login</button>
-		</div>
-
-		</form>
-		</div>
-		</div>
-		</div>
-	);
+            <div className="wrapper">
+              <button className="loginButton" type="submit">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateUsernamePage;
