c38c1d431436c92a5382e29215e5651eaecf521a
nirvanafan205 <nirvanafan205@gmail.com>
2023-08-15 17:37:04 -0700

    fixed form layout

--- src/CreateUsernamePage.jsx
+++ src/CreateUsernamePage.jsx
@@ -1,47 +1,23 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import "./style.css";
 
 const CreateUsernamePage = () => {
-  const [username, setUsername] = useState("");
-  const [isUsernameValid, setIsUsernameValid] = useState(true);
-  const [showErrorMessage, setShowErrorMessage] = useState(false);
-
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [isPasswordValid, setIsPasswordValid] = useState(true);
-  const [showPasswordErrorMessage, setShowPasswordErrorMessage] =
-    useState(false);
+  const navigate = useNavigate();
 
-  const handleUsernameChange = (event) => {
-    const newUsername = event.target.value;
-    const isValid = /^[a-z]*\d{3,}$/.test(newUsername);
-
-    setUsername(newUsername);
-    setIsUsernameValid(isValid);
-    setShowErrorMessage(!isValid && newUsername !== "");
-
-    const lowercaseLettersOnly = /^[a-z]+$/.test(newUsername);
-    const atLeastThreeNumbers = (newUsername.match(/\d/g) || []).length >= 3;
-    setIsUsernameValid(lowercaseLettersOnly && atLeastThreeNumbers);
-  };
-
-  const handlePasswordChange = (event) => {
-    const newPassword = event.target.value;
-    const isValid = /^[a-z]*\d+$/.test(newPassword); // Validate lowercase letters and at least one number
-
-    setPassword(newPassword);
-    setIsPasswordValid(isValid);
-    setShowPasswordErrorMessage(newPassword !== "" && !isValid);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (!isUsernameValid) {
-      return;
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/register", { name, password })
+      .then((result) => {
+        console.log(result);
+        navigate("/login");
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -54,51 +30,39 @@ const CreateUsernamePage = () => {
       </div>
 
       <div className="login-form">
-        <div className="color">
-          <h1 className="LoginPost">Registration</h1>
-          <form action="#" method="post">
-            <div className="inputWrapper">
-              <p>User Name</p>
-              <input
-                className={`input${!isUsernameValid ? " invalid" : ""}`}
-                type="text"
-                name="user"
-                placeholder="User Name"
-                value={username}
-                onChange={handleUsernameChange}
-              />
+        <h2>Register</h2>
 
-              <p>Password</p>
-              <input
-                className={`input${!isPasswordValid ? " invalid" : ""}`}
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="User Name"
+              autoComplete="off"
+              name="email"
+              className=""
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
 
-            {showErrorMessage && (
-              <p className="error-message">
-                User name must contain only lowercase letters and at least three
-                numbers.
-              </p>
-            )}
+          <div>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-            {showPasswordErrorMessage && (
-              <p className="error-message">
-                Password must contain lowercase letters and at least one number.
-              </p>
-            )}
+          <button type="submit" className="">
+            Register
+          </button>
 
-            <div className="wrapper">
-              <button className="loginButton" type="submit">
-                Login
-              </button>
-            </div>
-          </form>
-        </div>
+          <p>Already Have an Account</p>
+
+          <Link to="/login" className="link">
+            Login
+          </Link>
+        </form>
       </div>
     </div>
   );
--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons"; // Import the icon
 import "./style.css";
 
-
 const LoginPage = () => {
   return (
     <div>
@@ -13,32 +12,41 @@ const LoginPage = () => {
         <Link to="/" className="home-button">
           <FontAwesomeIcon icon={faHouse} />
         </Link>
-      </div>``
+      </div>
 
       {/* Login Form */}
       <div className="login-form">
         <div className="color">
           <h1 className="LoginPost">Login Form</h1>
           <form action="#" method="post">
-            <div className="inputWrapper" >
+            <div className="inputWrapper">
               <p>User Name</p>
-              <input className="input" type="text" name="user" placeholder="User Name" />
+              <input
+                className="input"
+                type="text"
+                name="user"
+                placeholder="User Name"
+              />
 
               <p>Password</p>
-              <input className="input" type="password" name="password" placeholder="Password" />
+              <input
+                className="input"
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
             </div>
 
             <div className="wrapper">
-              <button className="loginButton" type="submit">Login</button>
+              <button className="loginButton" type="submit">
+                Login
+              </button>
             </div>
-
           </form>
         </div>
       </div>
     </div>
   );
 };
-
-
 
 export default LoginPage;
