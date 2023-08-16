b4e99428e13e8a563339edd8a96466fb06b1ac93
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-08-16 13:00:37 -0700

    added comments

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,31 @@
+// import Mongoose library
+// used to work with MongoDB in a strucuted and object-oriented way
 const mongoose = require("mongoose");
 
+// defines a Mongoose scheme named userSchema
+// schema is a blueprint for how the data should be structured in the database collection
+// defines two fields: name and password, both of which are a type of string
 const userSchema = new mongoose.Schema({
   name: String,
   password: String,
 });
 
+// mongoose.model function is used to create a model for a MongoDB collection based on the schema
+// model is named userModel and corresponds to the users collection
+// first argument is the name of the collection
+// second argument is the schema that defines the strucute of the documents in the collection
 const userModel = mongoose.model("users", userSchema);
 
+//exportts the userModel so that it can be used in other parts of the lab
+// allos other modules to interact with the users collection using the defined schema and model
 module.exports = userModel;
+
+/*
+ 
+ 	This code defines a Mongoose schema for users collection w/ fields for the user's name and password
+
+	creates a model based on this schema and exports it for use in other parts of the application
+
+	allows CRUD ( Create, Read, Update, Delete) operation to be performed on the 
+	users collection using a structured and consistent API
+*/
--- server/server.js
+++ server/server.js
@@ -1,21 +1,54 @@
+// import modules are imported
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userModel = require("./models/user");
 
+// instance of express application is created
+// assigned it to the app varaible
 const app = express();
-app.use(express.json());
-app.use(cors());
 
+// two middleware functions used
+app.use(express.json()); // middleware parses incoming JSON payloads making it available in req.body for route handlers
+app.use(cors()); // enables Cross-Origin resource Charing
+// allos frontend to make requests to this server from a different origin
+
+// establishes a connection to the MongoDB data base
+// // uses IP address of the local machine 27017 ( default port )
+// /Users specifies the name of the database to connect to
 mongoose.connect("mongodb://127.0.0.1:27017/Users");
 
+// defins an API Endpoint
+// when a POST request is made to /register
+// calback function will be executed
 app.post("/register", (req, res) => {
+  /*
+    creates a new user in the database
+    req.body contains data sent w/ POST request
+    includes the user's info
+
+    .then() handles successful creation of the user
+    responds w/ created user object in JSON format using res.json(user)
+
+    .catch() handles erros and respons w/ error in JSON format using res.json(err)
+   */
   userModel
     .create(req.body)
     .then((users) => res.json(users))
     .catch((err) => res.json(err));
 });
 
+// starts the server
+// listens on port 3001
+// callback function will be executed once the server is up and runnign
 app.listen(3001, () => {
   console.log("server is running");
 });
+
+/*
+    Code defines an Express server that listens for POST request on the /register endpoint
+
+  when a request is made, it creates a new user in the MongoDB database using the userModel
+
+  created user is then sent as a JSON response
+*/
